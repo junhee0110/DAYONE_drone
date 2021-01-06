6e8a888d33d6062d8411c30d167bbf579e70aac9
Lee Junhee <junhee0110@gmail.com>
2021-01-06 19:10:47 +0900

    Re-design sensor class

--- main/sensor.h
+++ main/sensor.h
@@ -10,47 +10,11 @@
 #include <Wire.h>
 #endif
 
-#define HMC5883L_DEFAULT_ADDRESS    0x1E
-#define HMC5883L_RA_DATAX_H         0x03
-#define HMC5883L_RA_DATAZ_H         0x05
-#define HMC5883L_RA_DATAY_H         0x07
-
-#define MOVAVG_SIZE             32
-#define STANDARD_SEA_PRESSURE  1013.25
-
-#define INTERRUPT_PIN 2
-
 class gy86 : public MPU6050, public MS561101BA {
 
-  public:
-    //Common part
-    gy86();//initialize
-    void init();//Setup
-    void get_state();//Update state
-
-    float get_yaw();
-    float get_pitch();
-    float get_roll();
-    float get_alt();
-    float get_head();
-
-    //Barometer
-    float getAltitude();//get altitude using temp and pressure
-    void pushAvg(float val);//to use mean filter, update array
-    float getAvg(float *buff, int size);//mean filter
-    //Gyro
-    //EMPTY
-
   private:
-    float yaw, pitch, roll;
-
-    bool blinkState = false;
-    bool mpu_connection;
-    float heading, yaw_base;
-    float altitude, temperature, pressure;
-
-
     // MPU control/status vars
+    bool test_connection = false;
     bool dmpReady = false;  // set true if DMP init was successful
     uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
     uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
@@ -61,21 +25,20 @@ class gy86 : public MPU6050, public MS561101BA {
     // orientation/motion vars
     Quaternion q;           // [w, x, y, z]         quaternion container
     VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+    VectorInt16 gy;         // [x, y, z]            gyro sensor measurements
     VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
     VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
     VectorFloat gravity;    // [x, y, z]            gravity vector
     float euler[3];         // [psi, theta, phi]    Euler angle container
     float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-    volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-
+  
     // packet structure for InvenSense teapot demo
-    uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
-
-    float press_buff[MOVAVG_SIZE];
-    int press_avg_i = 0;
-    const float sea_press = 1026.4;
-
+    uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' }; 
+  
+  public:
+    void init();
+   
 };
 
 
--- main/sensor.ino
+++ main/sensor.ino
@@ -1,165 +1,32 @@
 #include "sensor.h"
 
-
-gy86::gy86() {
-  MPU6050();
-  MS561101BA();
-}
-
-void gy86::init() {
+void gy86::init(){
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+    Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+  #endif
 
   MPU6050::initialize();
-#ifdef INTERRUPT
-  pinMode(INTERRUPT_PIN, INPUT);
-#endif
-  mpu_connection = MPU6050::testConnection();
-
-  //To directly Read/Write sub I2C bus, Enable BypassMode
-  MPU6050::setI2CMasterModeEnabled(0);
-  MPU6050::setI2CBypassEnabled(1);
-
-  // 연속 읽기 모드로 설정
-  Wire.beginTransmission(HMC5883L_DEFAULT_ADDRESS);
-  Wire.write(0x02);
-  Wire.write(0x00);
-  Wire.endTransmission();
-  delay(5);
-
-  // 측정 간격을 75Hz
-  Wire.beginTransmission(HMC5883L_DEFAULT_ADDRESS);
-  Wire.write(0x00);
-  Wire.write(B00011000);
-  Wire.endTransmission();
-  delay(5);
-
-  MPU6050::setI2CBypassEnabled(0);
-
-  //Initializing DMP6
+  test_connection = MPU6050::testConnection();
   devStatus = MPU6050::dmpInitialize();
 
-  //Set gyro offset
-  MPU6050::setXGyroOffset(220);
-  MPU6050::setYGyroOffset(76);
-  MPU6050::setZGyroOffset(-85);
-  MPU6050::setZAccelOffset(1788);
+  // supply your own gyro offsets here, scaled for min sensitivity
+  MPU6050::setXGyroOffset(51);
+  MPU6050::setYGyroOffset(8);
+  MPU6050::setZGyroOffset(21);
+  MPU6050::setXAccelOffset(1150);
+  MPU6050::setYAccelOffset(-50);
+  MPU6050::setZAccelOffset(1060);
 
-  //re-check that it worked well
+   // make sure it worked (returns 0 if so)
   if (devStatus == 0) {
-    MPU6050::CalibrateAccel(6);
+    // Calibration Time: generate offsets and calibrate our MPU6050
+    CalibrateAccel(6);
     MPU6050::CalibrateGyro(6);
     MPU6050::PrintActiveOffsets();
-
+    // turn on the DMP, now that it's ready
     MPU6050::setDMPEnabled(true);
-
-#ifdef INTERRPUT
-    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN),  dmpDataReady, RISING);
-#endif
-
-    mpuIntStatus = MPU6050::getIntStatus();
-    dmpReady = true;
-
-    packetSize = MPU6050::dmpGetFIFOPacketSize();
-  }
-
-  else {
-    //Error
-    // 1 = initial memory load failed
-    // 2 = DMP configuration updates failed
-
-    //HAVE TO DISCUSS
-    //HOW TO SEND ERROR MESSAGE
-  }
-
-  yaw_base = atan2(MPU6050::getExternalSensorWord(2), MPU6050::getExternalSensorWord(0));
-  if (yaw_base < 0) yaw_base += 2 * M_PI;
-
-#ifdef MPU_LED
-  pinMode(MPU_LED, OUTPUT);
-#endif
-}
-
-void gy86::get_state() {
-
-DMP6:
-  if (!dmpReady) goto HMC; // if programming failed, don't try to do anything
-  if (MPU6050::dmpGetCurrentFIFOPacket(fifoBuffer)) {
-
-    MPU6050::dmpGetQuaternion(&q, fifoBuffer);
-    MPU6050::dmpGetGravity(&gravity, &q);
-    MPU6050::dmpGetYawPitchRoll(ypr, &q, &gravity);
-    pitch = ypr[1] * 180 / M_PI, roll = ypr[2] * 180 / M_PI; //Yaw => HMC
-
-    //#ifdef REALACCEL
-    MPU6050::dmpGetLinearAccel(&aaReal, &aa, &gravity);
-    //#endif
-
-#ifdef MPU_LED
-    blinkState = !blinkState;
-    digitalWrite(MPU_LED, HIGH);
-#endif
-  }
-
-HMC:
-  heading = atan2(MPU6050::getExternalSensorWord(2), MPU6050::getExternalSensorWord(0));
-  if (heading < 0) heading += 2 * M_PI;
-
-  yaw = heading - yaw_base;
-  if (yaw < 0) yaw += 2 * M_PI;
-  if (yaw > 180) yaw -= 2 * M_PI; // yaw = -180~180
-
-  yaw *= (180 / M_PI);
-ALT:
-  altitude = getAltitude();
-
-}
-
-float gy86::getAltitude() {
-  float temp;
-  temp = MS561101BA::getTemperature();
-  if (temp) {
-    temperature = temp;
-  }
-  delay(100);
-
-  pressure = MS561101BA::getPressure();
-
-  if (pressure != NULL) {
-    pushAvg(pressure);
-  }
-
-  pressure = getAvg(press_buff, MOVAVG_SIZE);
-
-  return ((pow(sea_press / pressure, 1 / 5.257) - 1) * (temperature + 273.15)) / 0.0065;
-
-}
-
-void gy86::pushAvg(float val) {
-  press_buff[press_avg_i] = val;
-  press_avg_i = (press_avg_i + 1) % MOVAVG_SIZE;
-}
-
-float gy86::getAvg(float *buff, int size) {
-  float sum = 0.0;
-  for (int i = 0; i < size; i++) {
-    sum += buff[i];
-  }
-
-  return sum / size;
-}
-
-
-float gy86::get_yaw() {
-  return yaw;
-}
-float gy86::get_pitch() {
-  return pitch;
-}
-float gy86::get_roll() {
-  return roll;
-}
-float gy86::get_alt() {
-  return altitude;
-}
-float gy86::get_head() {
-  return heading;
 }
