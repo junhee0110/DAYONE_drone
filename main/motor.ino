985b46beb2f372441cddfa6055ba6b0540debfb8
Lee Junhee <junhee0110@gmail.com>
2021-02-09 19:41:35 +0900

    Add motor.ino, motor.h

--- main/motor.ino
+++ main/motor.ino
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // File name: motor.ino
 // Purpose: control motor
 // Author: Lee Junhee, Hong Miru
@@ -16,37 +15,4 @@ void motor::init(){
 
 void motor::rotate(int value){
     Servo::writeMicroseconds(value);
-=======
-// File name : motor.ino
-// Last Update : 2021-02-09
-// Purpose : Control Motor
-// Author : Hong Miru
-//------------- Edit log --------------------
-
-#include "config.h"
-#include "control.h"
-#include <Servo.h> //라이브러리 불러오기
-Servo bldc ; //모터 선언
-Serial.begin(9600) ;
-bldc.attach(9);
-//------------------------------------------ㅡMOTOR_CONTROL-------------------------------------
-
-motor_control::motor_control(int pin, int motor){
-  int temp = motor_num;
-  pin_num = pin_num;
-  motor_num = motor_num;
-  x_sign = (bool)(temp%2);
-  y_sign = (bool)((temp-(temp%2))/2);
-}
-
-void motor_control::cal_output(axis &x, axis &y) {
-  this -> output_val += ( (this-> x_sign ? -1 * x.cal_PID() : x.cal_PID() ) + (this-> y_sign ? -1 * y.cal_PID() : y.cal_PID() ) );
-}
-
-void motor_control::output() {
-//servo라이브러리로 BLDC모터 제어
-
-  bldc.write(pos);
-  Serial.println(pos);
->>>>>>> 2b12d49d7d5fb19bae2d4828ec90b2b45553d3c5
 }
